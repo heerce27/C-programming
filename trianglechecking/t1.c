9d556e6eac5f956612efec6ceddff984d2560023
HEER SHAH <heershah.ce@gmail.com>
2025-08-24 17:31:08 +0530

    Add files via upload

---
+++ trianglechecking/t1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a1,a2,a3;
+    printf("Enter angles of triangle:");
+    scanf("%d %d %d",&a1,&a2,&a3);
+    if(a1+a2+a3==180)
+        printf("Valid triangle");
+    else
+        printf("Invalid triangle");
+}
