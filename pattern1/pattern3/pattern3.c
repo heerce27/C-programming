7c932694ebbca8bc09df4ee62cb096f4eadac1cf
HEER SHAH <heershah.ce@gmail.com>
2025-08-26 12:42:07 +0530

    Update pattern3.c

--- pattern1/pattern3/pattern3.c
+++ pattern1/pattern3/pattern3.c
@@ -4,15 +4,18 @@
       */
 #include<stdio.h>
 void main()
-{
-    for(int i=1;i<=5;i++)
+{ int n;
+ printf("Enter n:");
+ scanf("%d",&n);
+    for(int i=1;i<=n;i++)
     {
         for(int j=1;j<i;j++)
         printf(" ");
-        for(int j=1;j<=5-i+1;j++)
+        for(int j=1;j<=n-i+1;j++)
         {
             printf("%d",j);
         }
         printf("\n");
     }
 }
+
