ac7069c53afb63ce34a619f3d934e493d6a6398f
HEER SHAH <heershah.ce@gmail.com>
2025-09-16 00:41:55 +0530

    Add files via upload

---
+++ menudriven/menudriven.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int n,ch,i,sum=0;;
+    do
+    {
+        printf("---Menu--");
+        printf("\n1.Perfect number");
+        printf("\n2.Prime number");
+        printf("\n3.Factorial number");
+        printf("\n4.Exit");
+        printf("\nEnter your choice:");
+        scanf("%d",&ch);
+        switch(ch)
+    case 1:
+        printf("--Prime number or not--");
+        printf("Enter a number:");
+        scanf("%d",&n);
+
+    }
+}
